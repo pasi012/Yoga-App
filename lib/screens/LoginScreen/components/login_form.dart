c878be31cad2e16b0659325a2219ec31ab7032c7
pasi012 <s.a.pasindupiyushan@gmail.com>
2024-05-05 13:20:53 +0530

    Final Commit

--- lib/screens/LoginScreen/components/login_form.dart
+++ lib/screens/LoginScreen/components/login_form.dart
@@ -1,46 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:yoga/screens/HomeScreen/home_screen.dart';
 import '../../../constants.dart';
 import '../../input_field.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-// Future<void> login(String email, String password, BuildContext context) async {
-//   var url = '';
-//   var response = await http.post(
-//     Uri.parse(url),
-//     headers: {
-//       'base_url': '84.46.249.96:5000/api/v1/auth/login '  // Replace with your actual Postman API key
-//     },
-//     body: jsonEncode(<String, String>{
-//       'email': email,
-//       'password': password
-//     }),
-//   );
-//
-//   if (response.statusCode == 200) {
-//     var jsonResponse = jsonDecode(response.body);
-//     var token = jsonResponse['token'];
-//     print('Login successful: $token');
-//
-//     Navigator.pop(context);
-//     Navigator.push(
-//       context,
-//       MaterialPageRoute(
-//         builder: (context) {
-//           return const HomeScreen();
-//         },
-//       ),
-//     );
-//
-//   } else {
-//     print('Failed to login');
-//     // Handle error
-//   }
-// }
+void login(String email, String password, BuildContext context) async {
 
+  var url = 'http://84.46.249.96:5000/api/v1/auth/login';
+  var response = await http.post(
+    Uri.parse(url),
+    body: {'email': email, 'password': password},
+  );
 
+  if (response.statusCode == 200) {
+    var jsonResponse = jsonDecode(response.body);
+    var token = jsonResponse['token'];
+    print('Login successful: $token');
+
+    Fluttertoast.showToast(
+        msg: 'Login successful!',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+    Navigator.pop(context);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return const HomeScreen();
+        },
+      ),
+    );
+  } else {
+
+    Fluttertoast.showToast(
+        msg: 'Failed to login: Please check Email & Password',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+    print('Failed to login');
+    // Handle error
+  }
+}
 
 class LoginForm extends StatefulWidget {
   const LoginForm({
@@ -52,13 +64,12 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
-
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
 
-  // void _handleLogin() {
-  //   login(_emailController.text, _passwordController.text, context);
-  // }
+  void _handleLogin() {
+    login(_emailController.text, _passwordController.text, context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -114,19 +125,14 @@ class _LoginFormState extends State<LoginForm> {
               minimumSize: Size(double.infinity, 40.h),
             ),
             onPressed: () {
-              Navigator.pop(context);
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const HomeScreen();
-                  },
-                ),
-              );
+              _handleLogin();
             },
             child: Text(
               "Log in",
-              style: TextStyle(color: Colors.white, fontSize: 14.sp, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.bold),
             ),
           ),
         ],
--- lib/screens/RegisterScreen/components/signup_form.dart
+++ lib/screens/RegisterScreen/components/signup_form.dart
@@ -1,7 +1,82 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:yoga/screens/LoginScreen/login_screen.dart';
 import '../../../constants.dart';
 import '../../input_field.dart';
+import 'package:http/http.dart' as http;
+
+void register(String name, email, gender, password, BuildContext context) async {
+
+  var url = 'http://84.46.249.96:5000/api/v1/auth/register';
+  var response = await http.post(
+    Uri.parse(url),
+    body: {
+      "username": name,
+      "email": email,
+      "gender": gender,
+      "password": password
+    },
+  );
+
+  if (response.statusCode == 200) {
+
+    if(response.body.compareTo("email") != email){
+
+      var jsonResponse = jsonDecode(response.body);
+      var token = jsonResponse['token'];
+      print('Registration Successful: $token');
+
+      Fluttertoast.showToast(
+          msg: 'Registration Successful!',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+
+      Navigator.pop(context);
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return const LoginScreen();
+          },
+        ),
+      );
+
+    } else {
+
+      Fluttertoast.showToast(
+          msg: "User already exists!",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+
+    }
+
+  } else {
+
+    Fluttertoast.showToast(
+        msg: 'Failed to register: Please check all Field',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+
+  }
+
+}
 
 class SignUpForm extends StatefulWidget {
   const SignUpForm({
@@ -13,13 +88,16 @@ class SignUpForm extends StatefulWidget {
 }
 
 class _SignUpFormState extends State<SignUpForm> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _genderController = TextEditingController();
+  TextEditingController _ageController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _confirmPasswordController = TextEditingController();
 
-  TextEditingController emailController = TextEditingController();
-  TextEditingController nameController = TextEditingController();
-  TextEditingController genderController = TextEditingController();
-  TextEditingController ageController = TextEditingController();
-  TextEditingController confirmPasswordController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  void _handleRegister() {
+    register(_nameController.text, _emailController.text, _genderController.text, _passwordController.text, context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,13 +106,13 @@ class _SignUpFormState extends State<SignUpForm> {
         children: [
           MyInputField(
             title: "Email",
-            controller: emailController,
+            controller: _emailController,
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: defaultPadding/4),
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding / 4),
             child: MyInputField(
               title: "user name",
-              controller: nameController,
+              controller: _nameController,
             ),
           ),
           Row(
@@ -42,7 +120,7 @@ class _SignUpFormState extends State<SignUpForm> {
               Expanded(
                 child: MyInputField(
                   title: "gender",
-                  controller: genderController,
+                  controller: _genderController,
                 ),
               ),
               const SizedBox(
@@ -51,23 +129,25 @@ class _SignUpFormState extends State<SignUpForm> {
               Expanded(
                 child: MyInputField(
                   title: "age",
-                  controller: ageController,
+                  controller: _ageController,
                 ),
               ),
             ],
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: defaultPadding/4),
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding / 4),
             child: MyInputField(
               title: "password",
-              controller: passwordController,
+              controller: _passwordController,
             ),
           ),
           MyInputField(
             title: "confirm password",
-            controller: confirmPasswordController,
+            controller: _confirmPasswordController,
           ),
-          SizedBox(height: 100.h,),
+          SizedBox(
+            height: 100.h,
+          ),
           ElevatedButton(
             style: ElevatedButton.styleFrom(
               elevation: 0,
@@ -78,11 +158,28 @@ class _SignUpFormState extends State<SignUpForm> {
               minimumSize: Size(double.infinity, 40.h),
             ),
             onPressed: () {
-              //allowAdminToRegister();
+
+              if(_passwordController.text == _confirmPasswordController.text){
+                _handleRegister();
+              } else {
+                Fluttertoast.showToast(
+                    msg: "password not match!",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.BOTTOM,
+                    timeInSecForIosWeb: 1,
+                    backgroundColor: Colors.red,
+                    textColor: Colors.white,
+                    fontSize: 16.0
+                );
+              }
+
             },
             child: Text(
               "Create Account",
-              style: TextStyle(color: Colors.white, fontSize: 14.sp, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.bold),
             ),
           ),
         ],
